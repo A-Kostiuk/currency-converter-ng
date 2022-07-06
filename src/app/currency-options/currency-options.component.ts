6f660b370c22811202df02e04a053790ca70ea51
Andriy Kostiuk <Leo25334@gmail.com>
2022-07-06 10:23:49 +0300

    first commit

---
+++ src/app/currency-options/currency-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-options',
+  templateUrl: './currency-options.component.html',
+  styleUrls: ['./currency-options.component.scss']
+})
+export class CurrencyOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
